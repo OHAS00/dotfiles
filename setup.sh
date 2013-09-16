aad9a67bbfaa8110aae5c07767bda98f6b8c7e9b
OHAS00 <alone.hide.and.seek@gmail.com>
2013-09-16 16:56:11 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -fs $HOME/dotfiles/$file $HOME/$file
 done
 
-cp -a mytheme.sh ./tmux-powerline/themes/
+cp -af mytheme.sh ./tmux-powerline/themes/
+rm ../.gitignore
+rm ../.gitmodules
