d180d7e0eb2933a35d966e5a318710605af045da
OHAS00 <alone.hide.and.seek@gmail.com>
2015-02-21 15:28:23 +0900

    setup.sh add git setting

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git submodule init && git submodule update
+
 ln -fs $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -fs $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -fs $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
